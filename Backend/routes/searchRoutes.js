8be2adbaa2048211ff3a001b9fa76111a97b08f4
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2023-08-17 23:29:41 +0545

    testing

--- Backend/routes/searchRoutes.js
+++ Backend/routes/searchRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/search/general/',search.generalHandler);
 router.get('/search/organization_name/',search.nameHandler);
-router.get('/search/personnel/',search.personnelHandler);
+router.get('/search/personnel',search.personnelHandler);
 router.get('/search/program/',search.programHandler);
 
 
